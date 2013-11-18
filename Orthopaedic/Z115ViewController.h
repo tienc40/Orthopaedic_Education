c8685ae49a217c051d866ff2d9563de1277ee410
Z115 iOS & Web <z115.corporation@gmail.com>
2013-11-18 15:09:52 +0700

    no message

---
+++ Orthopaedic/Z115ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Z115ViewController.h
+//  Orthopaedic
+//
+//  Created by TienT on 11/18/13.
+//  Copyright (c) 2013 TienT. All rights reserved.
+//
+
+#import "ViewControllerWithNavbar.h"
+
+@interface Z115ViewController : ViewControllerWithNavbar
+
+@end
---
+++ Orthopaedic/Z115ViewController.m
@@ -0,0 +1,38 @@
+//
+//  Z115ViewController.m
+//  Orthopaedic
+//
+//  Created by TienT on 11/18/13.
+//  Copyright (c) 2013 TienT. All rights reserved.
+//
+
+#import "Z115ViewController.h"
+
+@interface Z115ViewController ()
+
+@end
+
+@implementation Z115ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
