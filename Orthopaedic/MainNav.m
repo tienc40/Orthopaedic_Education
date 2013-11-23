3127c6849c77304e82f715cb6e97b012c2a4ee90
Z115 iOS & Web <z115.corporation@gmail.com>
2013-11-23 23:35:46 +0700

    no message

--- Orthopaedic/MainNav.m
+++ Orthopaedic/MainNav.m
@@ -27,11 +27,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    NSLog(@"Loaded Cool");
-//    [[UINavigationBar appearance] setBarTintColor:[UIColor yellowColor]];
+    //[[UINavigationBar appearance] setBarTintColor:[UIColor yellowColor]];
     [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed:@"mainnavbg.png"] forBarMetrics:UIBarMetricsDefault];
     
-//    // font
+    // Font
     NSShadow *shadow = [[NSShadow alloc] init];
     shadow.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8];
     shadow.shadowOffset = CGSizeMake(0, 1);
@@ -40,22 +39,19 @@
     [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:[UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,shadow, NSShadowAttributeName,titleFont, NSFontAttributeName, nil]];
     
 
-    // image title
-//    
-//    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"mainnavtitle.png"]];
+    // Image Title
+    // self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"mainnavtitle.png"]];
 
 }
 
 - (void) viewWillAppear:(BOOL)animated
 {
     // add bar button
-
-    
 }
 //
 -(void)viewDidAppear:(BOOL)animated
 {
-    NSLog(@"did appear");
+
 }
 - (void)didReceiveMemoryWarning
 {
