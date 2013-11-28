e6cbcb5ae4e57731e1884254c040254ddd3f00cd
Z115 iOS & Web <z115.corporation@gmail.com>
2013-11-28 22:32:22 +0700

    no message

---
+++ Orthopaedic/Z115InfoViewController.h
@@ -0,0 +1,15 @@
+//
+//  Z115InfoViewController.h
+//  Orthopaedic
+//
+//  Created by TienT on 11/28/13.
+//  Copyright (c) 2013 TienT. All rights reserved.
+//
+
+#import "Z115ViewController.h"
+
+@interface Z115InfoViewController : Z115ViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *contactButton;
+
+@end
